98d47a63de9ff23b2b6af20c686a6855863dc22b
Adria Gil Sorribes <adriagil.94@gmail.com>
2025-02-13 19:04:46 +0000

    Extract Theme management to a separate component
     - By doing so we can make the layout a server side component
       which is the recommended to improve SEO and rendering of
       the site.
     - Move metadata back to layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,8 @@
-'use client';
-
 import localFont from 'next/font/local';
 import '@/styles/globals.css';
 import { Navbar } from '@/components/Navbar';
-import { createContext, useContext, useEffect, useState } from 'react';
-import { Button } from '@/components/ui/button';
-import { Moon, Sun } from 'lucide-react';
+import ThemeWrapper from '@/components/theme/ThemeWrapper';
+import { Metadata } from 'next';
 
 const geistSans = localFont({
   src: '../../public/fonts/GeistVF.woff',
@@ -18,68 +15,30 @@ const geistMono = localFont({
   weight: '100 900',
 });
 
-interface ThemeContextType {
-  isDark: boolean;
-  toggleTheme: () => void;
-}
-
-const ThemeContext = createContext<ThemeContextType>({
-  isDark: false,
-  toggleTheme: () => { },
-})
-
-function ThemeToggle() {
-  const { isDark, toggleTheme } = useContext(ThemeContext)
-
-  return (
-    <Button
-      variant='outline'
-      size='icon'
-      onClick={toggleTheme}
-      className='fixed top-4 right-4 bg-white text-indigo-500 dark:bg-gray-800'
-    >
-      {isDark ? <Sun className='h-[1.2rem] w-[1.2rem]' /> : <Moon className='h-[1.2rem] w-[1.2rem]' />}
-      <span className='sr-only'>Toggle theme</span>
-    </Button>
-  )
-}
+export const metadata: Metadata = {
+  title: 'Aviation Point',
+  description: 'Your hub for aviation!',
+  icons: {
+    icon: '/favicon.ico',
+    shortcut: '/favicon.ico',
+    apple: '/apple-touch-icon.png',
+  },
+};
 
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  const [isDark, setIsDark] = useState<boolean>(false);
-
-  useEffect(() => {
-    const savedTheme = localStorage.getItem('theme')
-    setIsDark(savedTheme === 'dark')
-  }, [])
-
-  useEffect(() => {
-    if (isDark) {
-      document.documentElement.classList.add('dark')
-      localStorage.setItem('theme', 'dark')
-    } else {
-      document.documentElement.classList.remove('dark')
-      localStorage.setItem('theme', 'light')
-    }
-  }, [isDark])
-
-  const toggleTheme = () => {
-    setIsDark(prev => !prev)
-  }
-
   return (
     <html lang='en'>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <ThemeContext.Provider value={{ isDark, toggleTheme }}>
+        <ThemeWrapper>
           <Navbar />
-          <ThemeToggle />
           {children}
-        </ThemeContext.Provider>
+        </ThemeWrapper>
       </body>
     </html>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,6 @@
 import { getAllPosts } from '@/lib/getPosts';
 import HomeClient from './page.client';
 import { BlogPost } from '@/types/blogPost';
-import { Metadata } from 'next';
-
-export const metadata: Metadata = {
-  title: 'Aviation Point',
-  description: 'Your hub for aviation!',
-  icons: {
-    icon: '/favicon.ico',
-    shortcut: '/favicon.ico',
-    apple: '/apple-touch-icon.png',
-  },
-};
-
 
 export default function Home() {
   const blogPosts: BlogPost[] = getAllPosts();
