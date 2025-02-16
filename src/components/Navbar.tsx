4dc86f95dce36bf26c659e169cf5d89f2fc2b08c
Adria Gil Sorribes <adriagil.94@gmail.com>
2025-02-16 16:16:01 +0000

    Add Log In & Sign Up buttons to NavBar component
     - Going to use NextAuth.js to implement the logic to
       login and create accounts for users.

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -24,7 +24,7 @@ export function Navbar() {
     <nav className='sticky top-0 z-100 bg-white dark:bg-gray-800 shadow-md'>
       <div className='max-w-7xl mx-auto px-4 sm:px-6 lg:px-8'>
         <div className='flex items-center justify-between h-16'>
-          <div className='flex items-center'>
+          <div className='flex items-center w-full'>
             <Link href='/' className='flex-shrink-0'>
               <Image
                 src='/logo-white.png'
@@ -51,10 +51,20 @@ export function Navbar() {
               </div>
             </div>
           </div>
+          <div className='hidden md:block'>
+            <div className='ml-10 flex items-baseline space-x-4'>
+              <Button variant='default' className='px-3 py-2 rounded-md text-sm font-medium bg-indigo-400 text-white hover:bg-indigo-500'>
+                Log In
+              </Button>
+              <Button variant='ghost' className='px-3 py-2 rounded-md text-sm font-medium text-gray-700 dark:text-gray-300 hover:bg-indigo-100 dark:hover:bg-gray-700'>
+                Sign Up
+              </Button>
+            </div>
+          </div>
           <div className='-mr-2 flex md:hidden'>
             <Sheet open={isOpen} onOpenChange={setIsOpen}>
               <SheetTrigger asChild>
-                <Button variant='ghost' className='inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500'>
+                <Button variant='ghost' className='inline-flex items-center justify-center p-2 rounded-md text-gray-800 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500'>
                   <span className='sr-only'>Open main menu</span>
                   {isOpen ? (
                     <X className='block h-6 w-6' aria-hidden='true' />
@@ -63,27 +73,37 @@ export function Navbar() {
                   )}
                 </Button>
               </SheetTrigger>
-              <SheetContent side='right' className='w-[240px] sm:w-[300px]'>
-                <div className='px-2 pt-2 pb-3 space-y-1 sm:px-3'>
-                  {navItems.map((item) => (
-                    <Link
-                      key={item.name}
-                      href={item.href}
-                      className={`block px-3 py-2 rounded-md text-base font-medium ${pathname === item.href
-                        ? 'bg-indigo-500 text-white'
-                        : 'text-gray-700 dark:text-gray-300 hover:bg-indigo-100 dark:hover:bg-gray-700'
-                        }`}
-                      onClick={() => setIsOpen(false)}
-                    >
-                      {item.name}
-                    </Link>
-                  ))}
+              <SheetContent side='right' className='w-[240px] sm:w-[300px] bg-white dark:bg-gray-800'>
+                <div className='px-2 pt-2 pb-3 space-y-1 sm:px-3 h-full flex flex-col justify-between'>
+                  <div className='mt-6 flex flex-col space-y-4'>
+                    {navItems.map((item) => (
+                      <Link
+                        key={item.name}
+                        href={item.href}
+                        className={`block px-3 py-2 rounded-md text-base font-medium ${pathname === item.href
+                          ? 'bg-indigo-400 text-white'
+                          : 'text-gray-700 dark:text-gray-300 hover:bg-indigo-100 dark:hover:bg-gray-700'
+                          }`}
+                        onClick={() => setIsOpen(false)}
+                      >
+                        {item.name}
+                      </Link>
+                    ))}
+                  </div>
+                  <div className='flex flex-col space-y-4 mt-6'>
+                    <Button variant='default' className='block px-3 py-2 rounded-md text-base font-medium bg-indigo-400 text-white hover:bg-indigo-500'>
+                      Log In
+                    </Button>
+                    <Button variant='ghost' className='block px-3 py-2 rounded-md text-base font-medium text-gray-700 dark:text-gray-300 hover:bg-indigo-100 dark:hover:bg-gray-700'>
+                      Sign Up
+                    </Button>
+                  </div>
                 </div>
               </SheetContent>
             </Sheet>
           </div>
         </div>
       </div>
-    </nav>
+    </nav >
   )
 }
